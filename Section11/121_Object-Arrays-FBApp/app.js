a1911e4d8c472fb9fe04464e2fafba18f091abf7
DSGQR <amini.daniel@gmail.com>
2020-06-18 17:04:08 -0700

    Add Track 121 Data Structure Facebook App Exercise

---
+++ Section11/121_Object-Arrays-FBApp/app.js
@@ -0,0 +1,26 @@
+let credentials = {
+	username : 'GQR',
+	password : 'secretpassword'
+};
+
+let database = [
+	{
+		username : 'GQR',
+		password : 'secretpassword'
+	}
+];
+
+let newsfeed = [
+	{
+		username : 'Jack',
+		timeline : 'Whats everyone doing tonight?'
+	},
+	{
+		username : 'Lexi',
+		timeline : 'Ready to party this weekend!'
+	},
+	{
+		username : 'Charlotte',
+		timeline : 'I cant wait to see the world!'
+	}
+];
